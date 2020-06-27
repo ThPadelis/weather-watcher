471ca69fb1135b40f76fbceeb674a60920af1627
unknown <padelis.theodosiou@gmail.com>
2020-06-27 17:22:55 +0300

    Testing

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const { getWeather } = require("./src/app");
 const cron = require("node-cron");
 const { logger } = require("./src/utils/logger");
 
-cron.schedule("0 0 * * *", async () => {
-  try {
-    const weather = await getWeather();
-    if (weather) {
-      logger.log({ level: "info", message: "Weather item saved" });
-    }
-  } catch (error) {
-    logger.error("Failed to save weather item");
-    logger.error(error);
-  }
+// cron.schedule("0 0 * * *", async () => {
+//   try {
+//     const weather = await getWeather();
+//     if (weather) {
+//       logger.log({ level: "info", message: "Weather item saved" });
+//     }
+//   } catch (error) {
+//     logger.error("Failed to save weather item");
+//     logger.error(error);
+//   }
+// });
+
+cron.schedule("* * * * *", async () => {
+  logger.log({ level: "info", message: "Weather item saved" });
 });
