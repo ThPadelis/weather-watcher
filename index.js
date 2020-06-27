7886e7945b9f2c5b2c2ac1b87c9c6f1faa0512a7
unknown <padelis.theodosiou@gmail.com>
2020-06-27 17:50:18 +0300

    Updated the application

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const { getWeather } = require("./src/app");
 const { logger } = require("./src/utils/logger");
 
-try {
-  const weather = await getWeather();
-  if (weather) {
-    console.info("Weather item saved");
-    logger.log({ level: "info", message: "Weather item saved" });
+(async function () {
+  try {
+    const weather = await getWeather();
+    if (weather) {
+      console.info("Weather item saved");
+      logger.log({ level: "info", message: "Weather item saved" });
+    }
+  } catch (error) {
+    console.error("Failed to save weather item");
+    console.error({ error });
   }
-} catch (error) {
-  console.error("Failed to save weather item");
-  console.error({ error });
-}
+})();
