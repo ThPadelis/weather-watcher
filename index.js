6c189d05c1e7d82de8438d7382c963ab496ea69c
ThPadelis <padelis.theodosiou@gmail.com>
2020-06-30 13:48:10 +0300

    Minor changes to check the Herkou Scheduler

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const { getWeather } = require("./src/app");
 const { logger } = require("./src/utils/logger");
 
-(async function () {
-  try {
-    const weather = await getWeather();
-    if (weather) {
-      console.info("Weather item saved");
-      logger.log({ level: "info", message: "Weather item saved" });
-    }
-  } catch (error) {
-    console.error("Failed to save weather item");
-    console.error({ error });
-  }
+// (async function () {
+//   try {
+//     const weather = await getWeather();
+//     if (weather) {
+//       console.info("Weather item saved");
+//       logger.log({ level: "info", message: "Weather item saved" });
+//     }
+//   } catch (error) {
+//     console.error("Failed to save weather item");
+//     console.error({ error });
+//   }
+// })();
+
+(function () {
+  logger.info("Application run!");
+  console.info("Application run!");
 })();
