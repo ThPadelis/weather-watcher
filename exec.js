4c7af0292b014f8d4e3db33c5ec5e04b157e82ca
ThPadelis <padelis.theodosiou@gmail.com>
2020-08-14 00:26:43 +0300

    Updates

--- exec.js
+++ exec.js
@@ -2,14 +2,21 @@ const { getWeather } = require("./src/app");
 const cron = require("node-cron");
 const { logger } = require("./src/utils/logger");
 
-(async function(){
+(async function () {
   try {
     const weather = await getWeather();
     if (weather) {
-      logger.log({ level: "info", message: "Weather item saved" });
+      console.log({
+        message: "Weather item saved",
+        createdAt: Date.now(),
+        item: JSON.stringify(weather),
+      });
     }
   } catch (error) {
-    logger.error("Failed to save weather item");
-    logger.error(error);
+    console.error({
+      message: "Failed to save weather item",
+      details: error.message,
+      createdAt: Date.now(),
+    });
   }
 })();
